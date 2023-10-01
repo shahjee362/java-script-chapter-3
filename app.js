69c5be22986c0f9d5cac98aadedfc590df139bc1
shahjee362 <ahmedshah222666@gmail.com>
2023-10-01 16:07:28 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var age = "I am 18 years old"
+
+alert(age)
+
+var information = "you  have visited this site 14 times"
+
+alert(information)
+
+var doc1 = "my birth year is 1990"
+
+var doc2 = "data type of my decleared varieble is number"
+
+document.write(doc1+"<br/>"+doc2)
+document.write("<br>","John Doe ordered 5 T-shirt(s) on XYZ Clothing store.")
